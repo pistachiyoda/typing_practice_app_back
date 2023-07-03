29ff15b1d1d65061bb055a23ce2689e2ea787591
mai <fujisawamai21@gmail.com>
2023-07-03 15:24:16 +0900

    NextからNextにAPIリクエストを送ってレスポンスを表示できるようにした

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3000', // フロントエンドをデプロイした後、そのURLに変更する
+  });
   await app.listen(3005);
 }
 bootstrap();
